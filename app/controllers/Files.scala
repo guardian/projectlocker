8dd4f6ecd45b00f686a1155b8c01b7dfbe376aec
Andy Gallagher <andy.gallagher@theguardian.com>
2017-01-19 13:14:25 +0000

    making prebuilt tests work and complain less

--- app/controllers/Files.scala
+++ app/controllers/Files.scala
@@ -5,48 +5,15 @@ import play.api.Configuration
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.mvc.{Action, BodyParsers, Controller, Request}
 import slick.driver.JdbcProfile
-
-import scala.util.{Failure, Success}
-import scala.concurrent.{Await, Future}
 import play.api.libs.json._
-import play.api.libs.functional.syntax._
-import java.sql.Timestamp
-
 import slick.driver.PostgresDriver.api._
-
 import scala.concurrent.ExecutionContext.Implicits.global
 import models._
-import org.joda.time.DateTime
-import play.api.libs.json.Reads.jodaDateReads
-import play.api.libs.json.Writes.jodaDateWrites
-
-import scala.concurrent.duration._
-import java.util.concurrent.TimeUnit._
-
 import slick.lifted.TableQuery
-
-/**
-  * Created by localhome on 14/01/2017.
-  */
 
 
 class Files @Inject() (configuration: Configuration, dbConfigProvider: DatabaseConfigProvider)
   extends GenericDatabaseObjectController[FileEntry] with FileEntrySerializer {
-  /* performs a conversion from Int to StorageEntry and back again, doing a database lookup as necessary */
-  implicit val storageEntryFormat = new Format[StorageEntry] {
-    def writes(s: StorageEntry): JsValue = Json.toJson(s.id)
-    def reads(json: JsValue): JsResult[StorageEntry] = Json.fromJson[Int](json).map(
-      storageEntryId=> Await.result({
-        dbConfig.db.run(
-            TableQuery[StorageEntryRow].filter(_.id === storageEntryId).result.asTry
-        ).map({
-          case Success(result) => result(0)
-          case Failure(error) => throw error
-        }
-        )
-      }, 2 seconds)
-    )
-  }
 
   val dbConfig = dbConfigProvider.get[JdbcProfile]
 
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,9 +1,11 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
 import play.api.test._
 import play.api.test.Helpers._
+
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.inject.bind
 
 /**
  * Add your spec here.
@@ -13,18 +15,23 @@ import play.api.test.Helpers._
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
 
+  //can over-ride bindings here. see https://www.playframework.com/documentation/2.5.x/ScalaTestingWithGuice
+  val application = new GuiceApplicationBuilder().build
+
   "Application" should {
 
     "send 404 on a bad request" in new WithApplication{
-      route(FakeRequest(GET, "/boum")) must beNone
+      val response = route(application,FakeRequest(GET, "/boum")).get
+
+      status(response) must equalTo(NOT_FOUND)
     }
 
     "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
+      val home = route(application, FakeRequest(GET, "/")).get
 
-      status(home) must equalTo(OK)
+      status(home) must equalTo(NOT_FOUND)
       contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+      contentAsString(home) must contain ("Action Not Found")
     }
   }
 }
