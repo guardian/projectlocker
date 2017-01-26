a7c8f1bc0bb359ce52c89317cffdee37e6137bcc
Andy Gallagher <andy.gallagher@theguardian.com>
2017-01-26 14:14:40 +0000

    working storagecontroller tests

--- scripts/blank_test_db_actions.sh
+++ scripts/blank_test_db_actions.sh
@@ -48,6 +48,7 @@ drop table "ProjectEntry" cascade;
 drop table "ProjectTemplate" cascade;
 drop table "ProjectType" cascade;
 drop table "StorageEntry" cascade;
+drop table "play_evolutions" cascade;
 EOF
 
 #for x in `ls ${BASEPATH}/../conf/evolutions/test/*.sql`; do
--- test/StorageControllerSpec.scala
+++ test/StorageControllerSpec.scala
@@ -2,24 +2,7 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import org.junit.runner._
 import org.specs2.runner._
-import play.api.db.slick.DatabaseConfigProvider
-import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.test.Helpers._
-import play.api.test._
-import play.api.inject.bind
-import testHelpers.TestDatabase
-import play.api.{Application, Logger}
-import scala.concurrent.{Await, Future}
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
-import akka.actor._
-import akka.stream.ActorMaterializer
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
 import play.api.libs.json._
 
 @RunWith(classOf[JUnitRunner])
