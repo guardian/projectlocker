dc74d5f3c898351c0dfbb3d28c4a52ac978e98db
Andy Gallagher <andy.gallagher@theguardian.com>
2017-02-26 11:09:41 +0000

    barebones react.js setup

---
+++ frontend/app/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+    render () {
+        return(
+            <div>
+            <p> Hello React!</p>
+            </div>
+            ) ;
+    }
+}
+
+render(<App/>, document.getElementById('app'));
