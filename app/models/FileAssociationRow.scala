010b3138df481c8dc327760815173916b275ca3f
Andy Gallagher <andy.gallagher@theguardian.com>
2018-02-14 08:48:34 +0000

    adding inline docs

--- app/models/FileAssociationRow.scala
+++ app/models/FileAssociationRow.scala
@@ -2,6 +2,10 @@ package models
 
 import slick.driver.PostgresDriver.api._
 
+/**
+  * This table maintains assocations between files and projects, i.e. which files are part of which project
+  * @param tag
+  */
 class FileAssociationRow(tag: Tag) extends Table[(Int,Int)](tag, "ProjectFileAssociation") {
   def id=column[Int]("id",O.PrimaryKey,O.AutoInc)
   def projectEntry = column[Int]("ProjectEntry")
