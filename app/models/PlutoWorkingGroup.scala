cd2a429cb372e7e6ffea193106b6dcc8157c80e5
Andy Gallagher <andy.gallagher@theguardian.com>
2018-03-10 22:53:04 +0000

    view controller for working group, switching to an explicit uuid column instead of a string

--- app/models/PlutoWorkingGroup.scala
+++ app/models/PlutoWorkingGroup.scala
@@ -1,5 +1,7 @@
 package models
 
+import java.util.UUID
+
 import play.api.Logger
 import play.api.libs.functional.syntax.unlift
 import play.api.libs.json.{JsPath, Reads, Writes}
@@ -9,10 +11,9 @@ import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
-
 import scala.concurrent.ExecutionContext.Implicits.global
 
-case class PlutoWorkingGroup (id:Option[Int], hide:Option[String], name:String, uuid:String) {
+case class PlutoWorkingGroup (id:Option[Int], hide:Option[String], name:String, uuid:UUID) {
   private val logger = Logger(getClass)
   /**
     *  writes this model into the database, inserting if id is None and returning a fresh object with id set. If an id
@@ -62,7 +63,7 @@ class PlutoWorkingGroupRow(tag:Tag) extends Table[PlutoWorkingGroup](tag, "Pluto
   def id = column[Int]("id",O.PrimaryKey, O.AutoInc)
   def hide = column[Option[String]]("s_hide")
   def name = column[String]("s_name")
-  def uuid = column[String]("s_uuid")
+  def uuid = column[UUID]("u_uuid")
 
   def * = (id.?, hide, name, uuid) <> (PlutoWorkingGroup.tupled, PlutoWorkingGroup.unapply)
 }
@@ -72,14 +73,14 @@ trait PlutoWorkingGroupSerializer extends TimestampSerialization {
     (JsPath \ "id").writeNullable[Int] and
     (JsPath \ "hide").writeNullable[String] and
       (JsPath \ "name").write[String] and
-      (JsPath \ "uuid").write[String]
+      (JsPath \ "uuid").write[UUID]
     )(unlift(PlutoWorkingGroup.unapply))
 
   implicit val workingGroupReads:Reads[PlutoWorkingGroup] = (
     (JsPath \ "id").readNullable[Int] and
     (JsPath \ "hide").readNullable[String] and
       (JsPath \ "name").read[String] and
-      (JsPath \ "uuid").read[String]
+      (JsPath \ "uuid").read[UUID]
     )(PlutoWorkingGroup.apply _)
 }
 
