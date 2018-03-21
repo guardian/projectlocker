fc04f7a162296fcb1d7def3f54d4a7e95cffaf9c
Andy Gallagher <andy.gallagher@theguardian.com>
2018-03-21 15:49:36 +0000

    using snapshots to limit size of leveldb journal

--- app/services/actors/MessageProcessorActor.scala
+++ app/services/actors/MessageProcessorActor.scala
@@ -50,10 +50,29 @@ class MessageProcessorActor @Inject()(configurationI: Configuration, actorSystem
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val db = dbConfigProvider.get[PostgresProfile].db
 
+  protected val snapshotInterval = configuration.getOptional[Long]("pluto.persistence-snapshot-interval").getOrElse(50L)
   val logger = Logger(getClass)
 
-  def updateState(event:MessageEvent): Unit =
+  /**
+    * add an event to the journal, and snapshot if required
+    * @param event event to add
+    */
+  def updateState(event:MessageEvent): Unit = {
     state = state.updated(event)
+    if(lastSequenceNr % snapshotInterval ==0 && lastSequenceNr!=0)
+      saveSnapshot(state)
+  }
+
+  /**
+    * Logs to the journal that this event has been handled, so it won't be re-tried
+    * @param evtAsObject event object
+    */
+  def confirmHandled(evtAsObject:  MessageEvent):Unit = {
+    persist(EventHandled(evtAsObject.eventId)){ handledEventMarker=>
+      state = state.removed(evtAsObject)
+      saveSnapshot(state)
+    }
+  }
 
   override def receiveRecover:Receive = {
     case evt:MessageEvent =>
@@ -71,6 +90,12 @@ class MessageProcessorActor @Inject()(configurationI: Configuration, actorSystem
   }
 
   override def receiveCommand: Receive = {
+    case SaveSnapshotSuccess(metadata)=>
+      logger.debug(s"Successfully saved snapshot: $metadata")
+      logger.debug(s"Now removing messages to sequence no ${metadata.sequenceNr} from journal")
+      deleteMessages(metadata.sequenceNr)
+    case SaveSnapshotFailure(metadata,error)=>
+      logger.error(s"Could not save snapshot ${metadata.sequenceNr} for ${metadata.persistenceId}: ",error)
     case retry: RetryFromState=>  //retry all events in journal
       logger.debug("initiating retry cycle")
       state.foreach{ self ! _._2 }
@@ -112,23 +137,16 @@ class MessageProcessorActor @Inject()(configurationI: Configuration, actorSystem
       sendProjectCreatedMessage(msgAsObject).map({
         case Right(_) =>
           logger.info(s"Updated pluto with new project ${msgAsObject.projectEntry.projectTitle} (${msgAsObject.projectEntry.id})")
-          persist(EventHandled(evtAsObject.eventId)){ handledEventMarker=>
-            state = state.removed(evtAsObject)
-            saveSnapshot(state)
-          }
+          confirmHandled(evtAsObject)
         case Left(true) =>
           logger.debug(s"will retry from state ")
         case Left(false) =>
           logger.error("Not retrying any more.")
-          persist(EventHandled(evtAsObject.eventId)){ handledEventMarker=>
-            state = state.removed(evtAsObject)
-            saveSnapshot(state)
-          }
+          confirmHandled(evtAsObject)
       }).recoverWith({
         case err: Throwable =>
           logger.error("Could not set up communication with pluto:", err)
-          logger.debug(s"requeueing message after $delay delay")
-          Future(actorSystem.scheduler.scheduleOnce(delay, self, evtAsObject))
+          Future(logger.debug(s"message will be requeued after $delay delay"))
       })
 
     case msgAsObject:NewAdobeUuid =>
