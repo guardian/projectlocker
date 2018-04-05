5bf6450e85d71a480323267c83b7b7aae8b15537
Andy Gallagher <andy.gallagher@theguardian.com>
2018-04-05 10:45:31 +0100

    implemented calling out to java classes as postruns

--- app/postrun/UpdatePremiereScratchpaths.scala
+++ app/postrun/UpdatePremiereScratchpaths.scala
@@ -1,6 +1,7 @@
 package postrun
 
 import helpers.PostrunDataCache
+import models.{PlutoCommission, PlutoWorkingGroup, ProjectEntry, ProjectType}
 
 import scala.util.{Failure, Success, Try}
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -8,7 +9,7 @@ import scala.concurrent.Future
 import scala.xml.transform.{RewriteRule, RuleTransformer}
 import scala.xml.{Elem, Node, Text}
 
-class UpdatePremiereScratchpaths extends AdobeXml {
+class UpdatePremiereScratchpaths extends PojoPostrun with AdobeXml {
   class UpdateAudioVideoLocations(newLocation:String,transferLocation:String) extends RewriteRule {
     override def transform(n: Node): Seq[Node] = n match {
       case Elem(prefix,"CapturedVideoLocation0",attributes,scope,_*)=>
@@ -22,32 +23,16 @@ class UpdatePremiereScratchpaths extends AdobeXml {
     }
   }
 
-//  def updateXmlData(elem: Elem, newLocation:String) = {
-//    Elem(
-//      elem.prefix,
-//      elem.label,
-//      elem.attributes,
-//      elem.scope,
-//      elem.child.map({
-//        case Elem(prefix,"CapturedVideoLocation0",attributes,scope,child@_*)=>
-//          Elem(prefix,"CapturedVideoLocation0",attributes,scope,Text(newLocation))
-//        case Elem(prefix,"CapturedAudioLocation0",attributes,scope,child@_*)=>
-//          Elem(prefix,"CapturedVideoLocation0",attributes,scope,Text(newLocation))
-//        case Elem(prefix,label,attributes,scope,child)=>
-//          Elem(prefix,label, attributes,scope,child)
-//      })
-//    )
-//
-//  }
-  def postrun(projectFile:String, projectFileExtension:String, dataCache: PostrunDataCache):Future[Try[PostrunDataCache]] = {
+  def postrun(projectFileName:String,projectEntry:ProjectEntry,projectType:ProjectType,dataCache:PostrunDataCache,
+              workingGroupMaybe: Option[PlutoWorkingGroup], commissionMaybe: Option[PlutoCommission]):Future[Try[PostrunDataCache]] = {
     val maybeNewPath = dataCache.get("created_asset_folder")
     if(maybeNewPath.isEmpty) Future(Failure(new RuntimeException("no value for created_asset_folder")))
 
-    getXmlFromGzippedFile(projectFile).map({
+    getXmlFromGzippedFile(projectFileName).map({
       case Failure(err)=>Failure(err)
       case Success(xmlData)=>
         val updatedXml = new RuleTransformer(new UpdateAudioVideoLocations(maybeNewPath.get, maybeNewPath.get)).transform(xmlData).head
-        putXmlToGzippedFile(projectFile,Elem.apply(updatedXml.prefix, updatedXml.label, updatedXml.attributes, updatedXml.scope, false, updatedXml.child :_*))
+        putXmlToGzippedFile(projectFileName,Elem.apply(updatedXml.prefix, updatedXml.label, updatedXml.attributes, updatedXml.scope, false, updatedXml.child :_*))
         Success(dataCache)
     })
   }
--- test/UpdatePremiereScratchpathsSpec.scala
+++ test/UpdatePremiereScratchpathsSpec.scala
@@ -1,21 +1,46 @@
 import java.io.File
 
 import helpers.PostrunDataCache
+import models.{ProjectEntry, ProjectType}
 import org.apache.commons.io.FileUtils
 import org.specs2.mutable.Specification
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
 import postrun.UpdatePremiereScratchpaths
+import slick.jdbc.JdbcProfile
+import testHelpers.TestDatabase
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
+import scala.util.Try
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class UpdatePremiereScratchpathsSpec extends Specification {
+  protected val application = new GuiceApplicationBuilder()
+    .overrides(bind[DatabaseConfigProvider].to[TestDatabase.testDbProvider])
+    .build
+
+  private val injector = application.injector
+
+  protected val dbConfigProvider = injector.instanceOf(classOf[DatabaseConfigProvider])
+  protected implicit val db = dbConfigProvider.get[JdbcProfile].db
+
   "UpdatePremiereScratchpaths.postrun" should {
     "correctly read in and update a gzipped xml file" in {
       FileUtils.copyFileToDirectory(new File("postrun/tests/data/blank_premiere_2017.prproj"), new File("/tmp"))
 
       val dataCache = PostrunDataCache(Map("created_asset_folder"->"/path/to/my/assets"))
       val s = new UpdatePremiereScratchpaths
-      val result = Await.result(s.postrun("/tmp/blank_premiere_2017.prproj","",dataCache),10 seconds)
+      val futureResults = Await.result(Future.sequence(Seq(
+        ProjectEntry.entryForId(1),
+        ProjectType.entryFor(1)
+      )), 10 seconds)
+
+      val pe = futureResults.head.asInstanceOf[Try[ProjectEntry]].get
+      val pt = futureResults(1).asInstanceOf[Try[ProjectType]].get
+
+      val result = Await.result(s.postrun("/tmp/blank_premiere_2017.prproj",pe,pt,dataCache,None,None),10 seconds)
       result must beSuccessfulTry
     }
   }
