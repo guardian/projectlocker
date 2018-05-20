9406343878507090753ce88f2b6ac30b6c64da27
Andy Gallagher <andy.gallagher@theguardian.com>
2018-05-20 18:51:32 +0100

    taking out postgres, adding in rpm-build package and cleaning up rpm cache when building test image

--- .circleci/containerscripts/javasbt.sh
+++ .circleci/containerscripts/javasbt.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
-yum -y install java-1.8.0-openjdk-devel
+yum -y clean all
+yum -y install java-1.8.0-openjdk-devel rpm-build postgresql
 curl -L https://dl.bintray.com/sbt/rpm/sbt-0.13.15.rpm > /tmp/sbt-0.13.15.rpm
 rpm -Uvh /tmp/sbt-0.13.15.rpm
 
+yum -y clean all
+rm -rf /var/cache/yum/*
 echo exit | sbt
-#mv /root/.ivy2 /home/circleci/.ivy2
-#chown -R circleci /home/circleci/.ivy2
