9305fa61acf4b434fc8ded9affaff9cf55934964
Andy Gallagher <andy.gallagher@theguardian.com>
2018-08-29 22:36:40 +0100

    experiment with CI docker build

--- .circleci/containerscripts/javasbtdockergit.sh
+++ .circleci/containerscripts/javasbtdockergit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 yum -y clean all
-yum -y install java-1.8.0-openjdk-devel rpm-build postgresql
+yum -y install java-1.8.0-openjdk-devel rpm-build postgresql git docker
 curl -L https://dl.bintray.com/sbt/rpm/sbt-1.1.4.rpm > /tmp/sbt-1.1.4.rpm
 rpm -Uvh /tmp/sbt-1.1.4.rpm
 
